126489669392e1989fc4673e2956878729bf647d
Gregory Haynes <greg@greghaynes.net>
2012-10-16 19:25:59 -0700

    Added routine to get useful words

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import Stemmer
 
 stemmy = Stemmer.Stemmer('english')
 
-def getCommonwords(path='data/commonwords.txt'):
+def getCommonWords(path='data/commonwords.txt'):
     '''So we dont classify common words'''
     words = {}
     with open(path) as f:
@@ -12,6 +12,17 @@ def getCommonwords(path='data/commonwords.txt'):
                 words[stemmed_word] = True
     return words
 
-if __name__ == '__main__':
-    print getCommonwords()
+common_words = getCommonWords()
 
+def getUsefulWords(text):
+    '''Grab a list of unique words where each word is not a common word'''
+    text.replace('\n', ' ')
+    all_words = text.split(' ')
+    useful_words_dict = {}
+    for word in all_words:
+        if len(word) > 0 and word not in common_words:
+            useful_words_dict[stemmy.stemWord(word)] = True
+    return useful_words_dict.keys()
+
+if __name__ == '__main__':
+    print getUsefulWords('This is a very neat piece of text')
