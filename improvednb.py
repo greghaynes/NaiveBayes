8ef2af106b2fa95224ce3fcee48c02c799a473c9
Gregory Haynes <greg@greghaynes.net>
2012-10-21 22:41:18 -0700

    Added training to improved naive bayes

--- improvednb.py
+++ improvednb.py
@@ -1,5 +1,23 @@
 import classifier
 
+class CategoryInfo(object):
+
+    def __init__(self):
+        self.word_counts = word_counts
+        self.total_word_count = total_word_count
+        self.document_count = 0
+
+    def handle_document(self, document):
+        words = document.split(' ')
+        for word in words:
+            self.handle_word(word)
+        self.document_count += 1
+
+    def handle_word(self, word):
+        word_count = self.word_counts.get(word, 0)
+        self.word_counts[word] = word_count + 1
+        self.total_word_count += 1
+
 class ImprovedNB(classifier.Classifier):
     '''An improved version of the naive bayes filter'''
 
@@ -7,12 +25,13 @@ class ImprovedNB(classifier.Classifier):
                  language='english',
                  common_words_path='data/commonwords.txt'):
         super(ImprovedNB, self).__init__(language, common_words_path)
+        self.categories = {}
 
     def text_category_probability(self, category, text):
         '''
         Probability of text being in specified category is calculated by:
         
-        e^( ln P(c) + sum of (ln P(T_k|c)) for all k - ln P(d))
+        e^(ln P(c) + sum of (ln P(T_k|c)) for all k - ln P(d))
         where:
             P(c) is the probability of the class
 
@@ -26,7 +45,13 @@ class ImprovedNB(classifier.Classifier):
         We also use a non-standard method to calculate P(T_k|c) in order to
         avoid overfitting (otherwise 0 values for a T_k will drive the whole
         solution to 0:
-            P(T_k|c) = 
+            P(T_k|c) = number of times T_k has appeared in class C docs + 1 /
+                       number of words in class C docs + 1
         '''
         pass
+ 
+    def train(self, category, text):
+        cat_info = self.categories.get(categories, CategoryInfo())
+        cat_info.handle_document(text)
+        self.categories[category] = cat_info
 
